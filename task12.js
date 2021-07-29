f34b80a8849b5b8794369d0a08df43472afc3703
Generethical <85286098+Generethical@users.noreply.github.com>
2021-07-29 17:00:38 +0300

    Update task12.js

--- task12.js
+++ task12.js
@@ -4,10 +4,10 @@
 //     Read about fetch(). Use it to send requests in your browser.
 //     Request example:
 //     http://geocode.xyz/Minsk?json=1&auth=<your_key_here>
-//         - Send parallel requests for information about cities - Minsk, Madrid, Rome. Display the result in format: city - country.
-// - Using Promise.race get the country of these cities - Paris, Nice. Display result. - Write a function that returns a promise that resolves
-// any cities' names after 3 second.
-// After you get names send parallel requests for information about cities. Display the country of the cities.
+// - Send parallel requests for information about cities - Minsk, Madrid, Rome. Display the result in format: city - country.
+// - Using Promise.race get the country of these cities - Paris, Nice. Display result.
+// - Write a function that returns a promise that resolves any cities' names after 3 second.After you get names send parallel requests for information about cities.
+//   Display the country of the cities.
 // - Handle errors
 
 const fetch = require("node-fetch");
@@ -15,11 +15,34 @@ const urlsToAll = ['http://geocode.xyz/Minsk?json=1&auth=153164625196849893706x3
               'http://geocode.xyz/Madrid?json=1&auth=153164625196849893706x36072',
               'http://geocode.xyz/Rome?json=1&auth=153164625196849893706x36072']
 
+function promiseALl(array){
+    array.map(url=>{
+    fetch(url, {
+        method: 'get',
+        dataType: 'json',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(response => { return response.json()}).then(responseData => {
+        return responseData
+    }).then(data=>{
+        console.log(`${data.standard.city} - ${data.standard.countryname}`)
+    }).catch(error=>console.log(error))
+})
+}
 
-Promise.all(urlsToAll.map(url => fetch(url).then(response => response.json()).then(data=>{
-    console.log(`${data.standard.city} - ${data.standard.countryname}`)
-}).catch(error=>{
-    console.error('Failed to fetch one or more of these URLs:');
-    console.log(urlsToAll);
-    console.error(error);
-})))
+function promiseRace(){
+    const country1 = new Promise(resolve => setTimeout(resolve,0,'Paris'))
+    const country2 = new Promise(resolve => setTimeout(resolve,0,'Nice'))
+    Promise.race([country1,country2]).then(value=>fetch(`http://geocode.xyz/${value}?json=1&auth=153164625196849893706x36072`)).then(response => { return response.json()}).then(responseData => {
+        return responseData
+    }).then(data=>{
+        console.log(`${data.standard.countryname}`)
+    }).catch(error=>console.log(error))
+}
+promiseALl(urlsToAll)
+promiseRace()
+
+
+
